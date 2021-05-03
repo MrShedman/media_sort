d043b818cb5fc0388d5aa2042aeced02b486d107
Ross Cunningham <roscoacer@googlemail.com>
2021-05-03 12:20:01 +0100

    Renamed to media_sort

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="image_sort",
+setup(name="media_sort",
       version="0.0.1",
       description="Image and video file sorter",
       author='Ross Cunningham',
+      author_email="roscoacer@googlemail.com",
+      url="https://github.com/MrShedman/media_sort",
       packages=find_packages(),
       install_requires=["Pillow", "filetype", "hachoir", "exifread"])
